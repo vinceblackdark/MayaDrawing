25da3c9264bd88737a5dffedd7633fd8c6303d29
Vince Black <127775448+vinceblackdark@users.noreply.github.com>
2023-04-28 18:55:34 +0200

    Create Circle.cs
    creates circle,
    remember your Console Buffer by your main window dimension

---
+++ Circle.cs
@@ -0,0 +1,42 @@
+    public class Circle
+    {
+        public char _circleChar { get; set; }
+        public int _radius { get; set; }
+        public string _Color { get; set; }
+        public int _posX { get; set; }
+        public int _posY { get; set; }
+
+        public bool _ShowCursor = Console.CursorVisible = false;
+        public Circle(int posY, int posX, int radius, string color, char circleChar)
+        {
+            _posY = posY;
+            _posX = posX;
+            _Color = color;
+            _radius = radius;
+            _circleChar = circleChar;
+        }
+
+        public void Draw()
+        {
+            for (int y = -_radius; y <= _radius; y++)
+            {
+                for (int x = -_radius; x <= _radius; x++)
+                {
+                    double distance = Math.Sqrt(x * x + (y * 2) * (y * 2));
+                    if (distance <= _radius)
+                    {
+                        MayaDrawing.consoleColor(_Color);
+                        MayaDrawing.consoleColor("defaultbg");
+                        MayaDrawing.CharToPosition(_posY + y, _posX + x / 3 - 1, _circleChar);
+                    }
+                    else
+                    {
+                        MayaDrawing.consoleColor("defaultbg");
+                        MayaDrawing.CharToPosition(_posY, _posX, ' ');
+                    }
+                }
+            }
+            _ShowCursor = false;
+            return;
+        }
+    }
