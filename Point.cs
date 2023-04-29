6a7cb89def0c30c325529fca90499f8003bc8193
Vince Black <127775448+vinceblackdark@users.noreply.github.com>
2023-04-29 19:06:45 +0200

    Update Point.cs

--- Point.cs
+++ Point.cs
@@ -1,99 +1,77 @@
-    public class Point
+public class Point
     {
-        private int _Width { get; set; }
-        private int _Height { get; set; }
-        public int _moveY { get; set; }
-        public int _moveX { get; set; }
-        public int _lastPosY { get; set; }
-        public int _lastPosX { get; set; }
-        public int _steps { get; set; }
+        private int _moveY;
+        private int _moveX;
+        private int _lastPosY;
+        private int _lastPosX;
         public int _posY { get; set; }
         public int _posX { get; set; }
         public string _Color { get; set; }
         public char _Style { get; set; }
-        public bool _isLastPosition { get; set; }
-
+        private bool _isLastPosition;
         public bool _ShowCursor = Console.CursorVisible = false;
         public Point(int posY, int posX, string color, char style)
         {
-            _Height = 1;
-            _Width = 1;
             _Color = color;
             _Style = style;
             _moveX = 0;
             _moveY = 0;
-            _steps = 1;
             _posY = posY;
             _posX = posX;
         }
-        public void Draw(Point point)
+        public void Draw()
         {
-            MayaDrawing.consoleColor(point._Color);
-            MayaDrawing.CharToPosition(point._posY, point._posX, point._Style);
+            MayaDrawing.consoleColor(_Color);
+            MayaDrawing.CharToPosition(_posY,_posX, _Style);
             Console.CursorVisible = false;
-            return;
         }
-        public void Move(Point point, string direction, int steps)
+        public void Move(string direction)
         {
+            int steps = 1;
+            _isLastPosition = true;
+
+            _posX = _lastPosX;
+            _posY = _lastPosY;
+
             switch (direction.ToString())
             {
                 case "left":
-                    if (point._posY <= 3) { return; }
+                    if (_posY <= 3) { return; }
                     else
                     {
-                        point._posY -= steps;
+                        _posY -= steps;
+                        _moveY -= steps;
                         break;
                     }
                 case "right":
-                    if (point._posY >= Console.WindowWidth - 4) { return; }
+                    if (_posY >= Console.WindowWidth - 4) { return; }
                     else
                     {
-                        point._posY += steps;
+                        _posY += steps;
+                        _moveY += steps;
                         break;
                     }
                 case "down":
-                    if (point._posX >= Console.WindowHeight - 4) { return; }
+                    if (_posX >= Console.WindowHeight - 4) { return; }
                     else
                     {
-                        point._posX += steps;
+                        _posX += steps;
+                        _moveX += steps;
                         break;
                     }
 
                 case "up":
-                    if (point._posX <= 2) { return; }
+                    if (_posX <= 2) { return; }
                     else
                     {
-                        point._posX -= steps;
+                        _posX -= steps;
+                        _moveX -= steps;
                         break;
                     }
                 default: break;
             }
-            Draw(point);
-            DeleteLast(direction, point._posY, point._posX);
-            Console.CursorVisible = false;
-            return;
+            Draw();
+            MayaDrawing.CharToPosition(_lastPosX, _lastPosY, Sprites._charEmpty);
+            Console.CursorVisible = false;  
         }
-        void DeleteLast(string direction, int posY, int posX)
-        {
-            switch (direction.ToString())
-            {
-                case "left":
-                    MayaDrawing.CharToPosition(posY + 1, posX, Sprites._charEmpty);
-                    break;
-                case "right":
-                    MayaDrawing.CharToPosition(posY - 1, posX, Sprites._charEmpty);
-                    break;
-                case "down":
-                    MayaDrawing.CharToPosition(posY, posX - 1, Sprites._charEmpty);
-                    break;
-                case "up":
-                    MayaDrawing.CharToPosition(posY, posX + 1, Sprites._charEmpty);
-                    break;
-                default:
-                    break;
-            }
-            Console.CursorVisible = false;
-            return;
-        }
-
     }
